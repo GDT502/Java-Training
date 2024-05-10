7b98520df5f129087f5cdfef2b75a9eae6935ef3
GDT502 <127180085+GDT502@users.noreply.github.com>
2024-05-10 09:27:50 +0000

    Java training Update - Graham

--- Hello World in Java/hello_world.java
+++ Hello World in Java/hello_world.java
@@ -1,5 +1,12 @@
 public class hello_world {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        int myFirstNumber = 10 + 5;
+        System.out.print(myFirstNumber);
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.print("Integer Minimum Value = " + myMinIntValue);
+        System.out.print("Integer Minimum = " + Integer.MIN_VALUE);
+        System.out.print("Integer Value Range (" + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE + ")" );
     }
 }
